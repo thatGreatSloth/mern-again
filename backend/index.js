a1aa2d1182f74cfb6208a31df19667668b1c3d7a
MsBookGurl <152986472+MsBookGurl@users.noreply.github.com>
2025-02-05 23:03:57 +1200

    "feat(auth): add authentication routes"

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from "express";
-import { connectDB } from "./db/connectDB.js";
 import dotenv from "dotenv";
+
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+
 
 dotenv.config(); //load environment variables
 const app = express();
@@ -8,6 +12,8 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+app.use("/api/auth", authRoutes)
 
 app.listen(3000, () => {
     connectDB();
