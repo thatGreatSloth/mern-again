1d1ea87cc0a1788794c4ed3da9d9c8c6cad589a6
MsBookGurl <152986472+MsBookGurl@users.noreply.github.com>
2025-02-05 23:08:51 +1200

    chore(auth-routes) : cleaned auth.route.js file

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+    res.send("Signup route");
+}
+export const login = async (req, res) => {
+    res.send("login route");
+}
+export const logout = async (req, res) => {
+    res.send("logout route");
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,16 +1,11 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-    res.send("Signup Route");
-})
-router.get("/login", (req, res) => {
-    res.send("Login Route");
-})
-router.get("/logout", (req, res) => {
-    res.send("Logout Route");
-})
+router.get("/signup", signup)
+router.get("/login", login)
+router.get("/logout", logout)
 
 
 export default router;
